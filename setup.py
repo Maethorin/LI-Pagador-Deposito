a88bcdb28fcf63983a511587e39dfa94fd3489aa
Márcio Duarte <maethorin@gmail.com>
2014-10-17 14:53:58 -0300

    Adicionando unicode

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='li-pagador-deposito',
-    version='0.0.5',
+    version='0.0.6',
     url='https://github.com/lojaintegrada/LI-Pagador-Deposito',
     license='MIT',
     description=u'Meio de pagamento usando depósito bancário',
