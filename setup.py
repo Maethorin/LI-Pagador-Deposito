3ee696af51161948cf822ee9e8943b6e5086e4c0
Jeferson Farias Calazans <calazans10@gmail.com>
2015-02-05 11:47:39 -0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='li-pagador-deposito',
-    version='1.0.5',
+    version='1.0.6',
     url='https://github.com/lojaintegrada/LI-Pagador-Deposito',
     license='MIT',
     description=u'Meio de pagamento usando depósito bancário',
