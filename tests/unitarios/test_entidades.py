5361a5b6d498f1509c8f013fd48f5ceb94f4ebdf
maethorin <maethorin@gmail.com>
2015-03-01 17:03:06 -0300

    Testanto malote

--- tests/unitarios/test_entidades.py
+++ tests/unitarios/test_entidades.py
@@ -103,3 +103,110 @@ class DepositoConfiguracaoMeioPagamento(unittest.TestCase):
             {'numero_conta': None, 'favorecido': None, 'imagem': 'imagem_2', 'nome': 'banco_2', 'operacao': None, 'poupanca': False, 'agencia': None, 'cpf_cnpj': None, 'codigo': 'codigo_2', 'ativo': False}
         ]
         configuracao.configurado.should.be.truthy
+
+    @mock.patch('pagador_deposito.reloaded.entidades.entidades.Banco')
+    @mock.patch('pagador_deposito.reloaded.entidades.ConfiguracaoMeioPagamento.preencher_do_gateway', autospec=True)
+    def test_deve_obter_banco_ativo(self, preencher_mock, banco_mock):
+        configuracao = entidades.ConfiguracaoMeioPagamento(234)
+        configuracao.json = [
+            {'id': 1, 'numero_conta': '4444', 'favorecido': 'ZES', 'imagem': 'imagem_13', 'nome': 'banco_3', 'operacao': None, 'poupanca': False, 'agencia': '555', 'cpf_cnpj': '1234568897', 'codigo': '303', 'ativo': True},
+            {'id': 2, 'numero_conta': '12322', 'favorecido': 'ZAS', 'imagem': 'imagem_1', 'nome': 'banco_1', 'operacao': None, 'poupanca': False, 'agencia': '1554', 'cpf_cnpj': '1234568', 'codigo': 'codigo_1', 'ativo': True},
+            {'id': 3, 'numero_conta': None, 'favorecido': None, 'imagem': 'imagem_2', 'nome': 'banco_2', 'operacao': None, 'poupanca': False, 'agencia': None, 'cpf_cnpj': None, 'codigo': 'codigo_2', 'ativo': False}
+        ]
+        configuracao.obter_banco_ativo('1').should.be.equal(configuracao.json[0])
+
+    @mock.patch('pagador_deposito.reloaded.entidades.entidades.Banco')
+    @mock.patch('pagador_deposito.reloaded.entidades.ConfiguracaoMeioPagamento.preencher_do_gateway', autospec=True)
+    def test_deve_obter_banco_ativo_retorna_erro_se_nao_for_ativo(self, preencher_mock, banco_mock):
+        configuracao = entidades.ConfiguracaoMeioPagamento(234)
+        configuracao.json = [
+            {'id': 1, 'numero_conta': '4444', 'favorecido': 'ZES', 'imagem': 'imagem_13', 'nome': 'banco_3', 'operacao': None, 'poupanca': False, 'agencia': '555', 'cpf_cnpj': '1234568897', 'codigo': '303', 'ativo': True},
+            {'id': 2, 'numero_conta': '12322', 'favorecido': 'ZAS', 'imagem': 'imagem_1', 'nome': 'banco_1', 'operacao': None, 'poupanca': False, 'agencia': '1554', 'cpf_cnpj': '1234568', 'codigo': 'codigo_1', 'ativo': True},
+            {'id': 3, 'numero_conta': None, 'favorecido': None, 'imagem': 'imagem_2', 'nome': 'banco_2', 'operacao': None, 'poupanca': False, 'agencia': None, 'cpf_cnpj': None, 'codigo': 'codigo_2', 'ativo': False}
+        ]
+        configuracao.obter_banco_ativo.when.called_with('3').should.throw(
+            entidades.ConfiguracaoBancoNaoEncontrada,
+            u'Não foi encontrado um banco ativo com id 3 nas configuracoes da loja 234'
+        )
+
+    @mock.patch('pagador_deposito.reloaded.entidades.entidades.Banco')
+    @mock.patch('pagador_deposito.reloaded.entidades.ConfiguracaoMeioPagamento.preencher_do_gateway', autospec=True)
+    def test_deve_obter_banco_ativo_retorna_erro_se_nao_achar(self, preencher_mock, banco_mock):
+        configuracao = entidades.ConfiguracaoMeioPagamento(234)
+        configuracao.json = [
+            {'id': 1, 'numero_conta': '4444', 'favorecido': 'ZES', 'imagem': 'imagem_13', 'nome': 'banco_3', 'operacao': None, 'poupanca': False, 'agencia': '555', 'cpf_cnpj': '1234568897', 'codigo': '303', 'ativo': True},
+            {'id': 2, 'numero_conta': '12322', 'favorecido': 'ZAS', 'imagem': 'imagem_1', 'nome': 'banco_1', 'operacao': None, 'poupanca': False, 'agencia': '1554', 'cpf_cnpj': '1234568', 'codigo': 'codigo_1', 'ativo': True},
+            {'id': 3, 'numero_conta': None, 'favorecido': None, 'imagem': 'imagem_2', 'nome': 'banco_2', 'operacao': None, 'poupanca': False, 'agencia': None, 'cpf_cnpj': None, 'codigo': 'codigo_2', 'ativo': False}
+        ]
+        configuracao.obter_banco_ativo.when.called_with('4').should.throw(
+            entidades.ConfiguracaoBancoNaoEncontrada,
+            u'Não foi encontrado um banco ativo com id 4 nas configuracoes da loja 234'
+        )
+
+
+class DepositoMontandoMalote(unittest.TestCase):
+    def setUp(self):
+        self.dados_json = [{
+            'id': str(i),
+            'ativo': i % 2 == 0,
+            'nome': 'Banco {}'.format(i),
+            'codigo': '{}{}{}'.format(i, i, i),
+            'imagem': 'imagem-{}.png'.format(i),
+            'agencia': '3645',
+            'numero_conta': '12345',
+            'poupanca': False,
+            'favorecido': 'Favorecido Nome {}'.format(i),
+            'operacao': None,
+            'cpf_cnpj': '12345678901',
+        } for i in range(1, 4)]
+        self.loja_id = 23
+        self.malote = entidades.Malote(mock.MagicMock(loja_id=self.loja_id, json=self.dados_json, email_comprovante='email@comprovante.com', informacao_complementar='informacao_complementar'))
+        self.pedido = mock.MagicMock()
+        self.pedido.numero = 1234
+        self.pedido.email_contato_da_loja = 'email@contato.com'
+
+    def test_deve_ter_propriedades(self):
+        entidades.Malote('configuracao').to_dict().should.be.equal(
+            {
+                'banco_agencia': None, 'banco_codigo': None, 'banco_imagem': None, 'banco_nome': None,
+                'numero_conta': None, 'operacao': None, 'eh_poupanca': False, 'cpf_cnpj': None,
+                'favorecido': None, 'informacao_complementar': None, 'email_comprovante': None
+            }
+        )
+
+    def test_deve_montar_conteudo(self):
+        dados = {'banco_id': '1'}
+        self.malote.configuracao.obter_banco_ativo.return_value = self.dados_json[0]
+        self.malote.monta_conteudo(self.pedido, parametros_contrato=None, dados=dados)
+        self.malote.to_dict().should.be.equal(
+            {
+                'banco_agencia': '3645', 'banco_codigo': '111', 'banco_imagem': 'imagem-1.png', 'banco_nome': 'Banco 1',
+                'numero_conta': '12345', 'operacao': None, 'eh_poupanca': False, 'cpf_cnpj': '123.456.789-01',
+                'favorecido': 'Favorecido Nome 1', 'informacao_complementar': 'informacao_complementar', 'email_comprovante': 'email@comprovante.com'
+            }
+        )
+
+    def test_obter_email_da_conta(self):
+        dados = {'banco_id': '1'}
+        self.malote.configuracao.obter_banco_ativo.return_value = self.dados_json[0]
+        self.malote.configuracao.email_comprovante = None
+        self.malote.monta_conteudo(self.pedido, parametros_contrato=None, dados=dados)
+        self.malote.to_dict().should.be.equal(
+            {
+                'banco_agencia': '3645', 'banco_codigo': '111', 'banco_imagem': 'imagem-1.png', 'banco_nome': 'Banco 1',
+                'numero_conta': '12345', 'operacao': None, 'eh_poupanca': False, 'cpf_cnpj': '123.456.789-01',
+                'favorecido': 'Favorecido Nome 1', 'informacao_complementar': 'informacao_complementar', 'email_comprovante': 'email@contato.com'
+            }
+        )
+
+    def test_da_erro_se_nao_passar_banco_id(self):
+        dados = {}
+        self.malote.monta_conteudo.when.called_with(self.pedido, parametros_contrato=None, dados=dados).should.throw(entidades.DepositoInvalido, u'Não foi informado o banco para o depósito do pedido {} no dados.'.format(self.pedido.numero))
+
+    def test_da_erro_se_nao_achar_banco(self):
+        dados = {'banco_id': '10'}
+        self.malote.configuracao.obter_banco_ativo.side_effect = entidades.ConfiguracaoBancoNaoEncontrada()
+        self.malote.monta_conteudo.when.called_with(self.pedido, parametros_contrato=None, dados=dados).should.throw(
+            entidades.DepositoInvalido,
+            u'O banco id {} para o depósito do pedido {} não está ativo na loja {}.'.format(10, self.pedido.numero, self.loja_id)
+        )
