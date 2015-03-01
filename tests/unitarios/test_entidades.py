e9eecf3dcdc952947169865ba02930a6d8d473bf
maethorin <maethorin@gmail.com>
2015-03-01 16:21:44 -0300

    Testes estavam indo no banco

--- tests/unitarios/test_entidades.py
+++ tests/unitarios/test_entidades.py
@@ -17,16 +17,19 @@ class DepositoConfiguracaoMeioPagamento(unittest.TestCase):
     def test_deve_ter_codigo_gateway(self):
         entidades.ConfiguracaoMeioPagamento._codigo_gateway.should.be.equal(self.codigo_gateway)
 
+    @mock.patch('pagador_deposito.reloaded.entidades.entidades.Banco', mock.MagicMock())
     @mock.patch('pagador_deposito.reloaded.entidades.ConfiguracaoMeioPagamento.preencher_do_gateway', autospec=True)
     def test_deve_preencher_do_gateway_na_inicializacao(self, preencher_mock):
         configuracao = entidades.ConfiguracaoMeioPagamento(234)
         preencher_mock.assert_called_with(configuracao, self.codigo_gateway, self.campos)
 
+    @mock.patch('pagador_deposito.reloaded.entidades.entidades.Banco', mock.MagicMock())
     @mock.patch('pagador_deposito.reloaded.entidades.ConfiguracaoMeioPagamento.preencher_do_gateway', autospec=True)
     def test_deve_definir_formulario_na_inicializacao(self, preencher_mock):
         configuracao = entidades.ConfiguracaoMeioPagamento(234)
         configuracao.formulario.should.be.a('pagador_deposito.reloaded.cadastro.FormularioDeposito')
 
+    @mock.patch('pagador_deposito.reloaded.entidades.entidades.Banco', mock.MagicMock())
     @mock.patch('pagador_deposito.reloaded.entidades.ConfiguracaoMeioPagamento.preencher_do_gateway', autospec=True)
     def test_deve_nao_ser_aplicacao(self, preencher_mock):
         configuracao = entidades.ConfiguracaoMeioPagamento(234)
