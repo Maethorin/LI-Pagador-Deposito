a94e3b404ffab4b6c505c238aaca2b142c92753e
maethorin <maethorin@gmail.com>
2015-02-28 23:43:36 -0300

    Refatorando nome de método de processa pagamento

--- src/pagador_deposito/reloaded/servicos.py
+++ src/pagador_deposito/reloaded/servicos.py
@@ -2,16 +2,10 @@
 from pagador.reloaded import servicos
 
 
-class Resultado(object):
-    def __init__(self, dados):
-        self.sucesso = True
-        self.conteudo = {'dados': dados}
-
-
 class EntregaPagamento(servicos.EntregaPagamento):
     def __init__(self, loja_id, plano_indice=1):
         super(EntregaPagamento, self).__init__(loja_id, plano_indice)
         self.tem_malote = True
 
-    def enviar_pagamento(self, tentativa=1):
-        self.resultado = Resultado(self.malote.to_dict())
+    def processa_dados_de_pagamento(self):
+        self.resultado = {'dados': self.malote.to_dict()}
