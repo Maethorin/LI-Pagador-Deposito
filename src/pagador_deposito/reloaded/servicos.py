71bb3ca29177ef76057ba029a4321252ad0b3188
maethorin <maethorin@gmail.com>
2015-03-05 17:24:47 -0300

    Passando dados para EntregaPagamento

--- src/pagador_deposito/reloaded/servicos.py
+++ src/pagador_deposito/reloaded/servicos.py
@@ -3,8 +3,8 @@ from pagador.reloaded import servicos
 
 
 class EntregaPagamento(servicos.EntregaPagamento):
-    def __init__(self, loja_id, plano_indice=1):
-        super(EntregaPagamento, self).__init__(loja_id, plano_indice)
+    def __init__(self, loja_id, plano_indice=1, dados=None):
+        super(EntregaPagamento, self).__init__(loja_id, plano_indice, dados=None)
         self.tem_malote = True
 
     def processa_dados_pagamento(self):
