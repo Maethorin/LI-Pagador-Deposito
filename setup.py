10a310b9b25fae172bb4252818295390e727d9cd
maethorin <maethorin@gmail.com>
2015-03-10 12:57:58 -0300

    Versionando

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='li-pagador-deposito',
-    version='2.0.1',
+    version='2.0.2',
     url='https://github.com/lojaintegrada/LI-Pagador-Deposito',
     license='MIT',
     description=u'Meio de pagamento usando depósito bancário',
