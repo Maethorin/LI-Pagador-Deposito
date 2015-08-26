9e53382fc656ad59f366c64c39bcaac7ee841da2
Márcio Duarte <maethorin@gmail.com>
2015-08-26 10:19:30 -0300

    Equalisando versões

--- setup.py
+++ setup.py
@@ -31,5 +31,5 @@ setup(
     package_dir={'': 'src'},
     include_package_data=True,
     package_data={'pagador_deposito': ['extensao/templates/*']},
-    install_requires=['distribute', 'li-pagador'],
+    install_requires=['distribute', 'li-pagador==2.1.1'],
 )
